ff8570003f0f4400f42b37513cfc72ce992e5004
adeilton-malafaia <adeiltonmalafaia@gmail.com>
2022-07-26 16:07:23 -0300

    Removendo contato e sobre

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes'
+#   'App 1',
+#   'App 2',
+#   (...),
+
 ]
 
 MIDDLEWARE = [
